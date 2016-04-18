31612ed3e694068486991fc705047d3077a31203
EliabLuvanda <luvanda@icloud.com>
2016-04-18 07:03:49 +0300

    ok

--- engel.R
+++ engel.R
@@ -3,5 +3,19 @@ rm(list = ls())
 setwd("~/engel_proj")
 library(haven)
 real_consaggr <- read_sav("real_consaggr.sav")
+attach(real_consaggr)
 View(real_consaggr)
 head(real_consaggr)
+
+# Number of all missing values in the data set
+sum(is.na(real_consaggr))
+
+# Creating value labels
+library(prettyR)
+real_consaggr$REGION <- add.value.labels(real_consaggr$REGION,
+                         c("Dodoma","Arusha","Kilimanjaro",
+                         "Tanga","Morogoro","Pwani",
+                         "Dar es Salaam","Lindi","Mtwara",
+                         "Ruvuma","Iringa","Mbeya","Singida",
+                         "Tabora","Rukwa","Kigoma","Shinyanga",
+                         "Kagera","Mwanza","Mara","Manyara"))
